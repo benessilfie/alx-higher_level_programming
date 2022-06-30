5c4a36561c46b4fca2401396b69e061f00cba45b
Essilfie <bquansah007@gmail.com>
2022-06-30 22:58:41 +0000

    add task 1

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from add_0 import add
+
+if __name__ == "__main__":
+    """
+    Prints the sum of the addition between two numbers
+    """
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The value of a + b
+    """
+    return (a + b)
