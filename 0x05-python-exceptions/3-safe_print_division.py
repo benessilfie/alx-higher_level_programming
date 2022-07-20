f5c61bf2ce03f610b43c3c4854fd79308b09d6b0
Essilfie <bquansah007@gmail.com>
2022-07-20 01:38:22 +0000

    add Python - Exceptions task

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    divide = 0
+
+    try:
+        divide = a / b
+    except ZeroDivisionError:
+        divide = None
+    finally:
+        print('Inside divide: {}'.format(divide))
+        return divide
