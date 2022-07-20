2f3ffb9fdee8e826f297f6395671c5a2b464ce98
Essilfie <bquansah007@gmail.com>
2022-07-20 01:50:23 +0000

    add Python - Exceptions task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 def safe_function(fct, *args):
+    import sys
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        result = None
+    return (result)
