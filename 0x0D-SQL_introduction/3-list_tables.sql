ec5b66d05aa7f87684da0ee14d2893aa841233cc
essilfiequansah <bquansah007@gmail.com>
2022-09-06 23:56:15 +0000

    add task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
 -- Lists all the tables of a database in MySQL Server.
---      The database name will be passed as argument by mysql command line.
+-- The database name will be passed as argument by mysql command line.
 SHOW TABLES;
